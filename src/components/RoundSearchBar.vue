651b986b03fe0dd49a472d26579b97715ad0971c
fuzzy <17826806525@163.com>
2019-05-23 17:23:06 +0800

    06 13:10

---
+++ src/components/RoundSearchBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="round-search-bar">
+    <input type="text" class="input-search">
+    <button class="btn-search">
+      <img class="icon-search" src="./../assets/icon_search.svg" alt>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "round-search-bar"
+};
+</script>
+
+<style lang="less" scoped>
+@import "./../themes/base.less";
+.round-search-bar {
+  .input-search {
+    height: @round-search-bar-height;
+    border-radius: @round-search-bar-border-radius;
+    border: 1px solid #dfe1e5;
+    padding: 20px;
+  }
+  .btn-search {
+    height: @round-search-bar-height;
+    .icon-search {
+      width: 30px;
+      height: 30px;
+    }
+  }
+}
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,5 +1,16 @@
-<template>  
+<template>
   <div class="search-page">
     <h1>This is an search page</h1>
+    <RoundSearchBar></RoundSearchBar>
   </div>
 </template>
+
+<script>
+// @ means "src"
+import RoundSearchBar from "@/components/RoundSearchBar";
+export default {
+  components: {
+    RoundSearchBar
+  }
+};
+</script>
